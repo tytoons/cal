3a66d85e0a9ac49a6ac0936ac5f4b8bf16767931
Tyler DeVos <tytoons@gmail.com>
2019-10-28 22:29:16 -0500

    Get app.service to run once instance boot

--- instance.py
+++ instance.py
@@ -1,11 +1,13 @@
+import sys
 import boto3
 ec2 = boto3.resource('ec2')
 
 # create a new EC2 instance
 instances = ec2.create_instances(
-     ImageId='ami-057b5be2c3b9cc63d',
-     MinCount=1,
-     MaxCount=1,
-     InstanceType='t3.nano',
-     KeyName='tytoons'
+     ImageId = sys.argv[1],
+     MinCount = 1,
+     MaxCount = 1,
+     InstanceType = 't3.nano',
+     KeyName = 'tytoons',
+     SecurityGroups = ['ec2_node_sg']
  )
